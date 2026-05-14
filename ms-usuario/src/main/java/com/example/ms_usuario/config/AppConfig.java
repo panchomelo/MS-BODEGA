1de6fb152371e1af4db4a3c2db029142e4f8013b
baolivaresm-droid <ba.olivaresm@duocuc.cl>
2026-05-13 20:10:26 -0400

    cambio de punto

--- ms-usuario/src/main/java/com/example/ms_usuario/config/AppConfig.java
+++ ms-usuario/src/main/java/com/example/ms_usuario/config/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 /**
- * Clase de configuración para el microservicio de usuarios.
+ * Clase de configuración para el microservicio de usuarios..
  * Define los componentes necesarios para la comunicación entre servicios.
  */
 @Configuration
