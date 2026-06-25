2c271b30a2a63969bf906353d2d52b83e4aa990a
baolivaresm-droid <ba.olivaresm@duocuc.cl>
2026-06-24 23:04:23 -0400

    Agregar Docker, YAML, Swagger y pruebas para ms-usuario

--- ms-usuario/src/main/java/com/example/ms_usuario/MsUsuarioApplication.java
+++ ms-usuario/src/main/java/com/example/ms_usuario/MsUsuarioApplication.java
@@ -2,12 +2,12 @@ package com.example.ms_usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // Requisito para Eureka (IE 3.3.2) [2]
 public class MsUsuarioApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsUsuarioApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MsUsuarioApplication.class, args);
+    }
 }
