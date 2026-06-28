ebdf56138491eb121324dc35246b7cf77a2fde72
baolivaresm-droid <ba.olivaresm@duocuc.cl>
2026-06-28 01:01:20 -0400

    feat: correccion de perfiles, ajuste de pool hikari para oracle cloud y registro exitoso en eureka

--- ms-eureka/src/main/java/com/example/Eureka/EurekaApplication.java
+++ ms-eureka/src/main/java/com/example/Eureka/EurekaApplication.java
@@ -2,8 +2,10 @@ package com.example.Eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer; // <-- Verifica esta importación
 
 @SpringBootApplication
+@EnableEurekaServer // <-- Esta anotación activa las funciones de servidor Eureka
 public class EurekaApplication {
 
 	public static void main(String[] args) {
